0f6425a3b6c7ddf9db85a43f31ecc4bd4c0be7c5
vinxtluvvv <Vinit.26420@stu.upes.ac.in>
2025-11-13 11:05:42 +0530

    Add files via upload

---
+++ Q46.c
@@ -0,0 +1,20 @@
+/*Q46 (Nested Loops without Arrays/Strings)
+Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+*/
+
+#include <stdio.h>
+
+int main(){
+    for (int i=1; i<=5;i++){
+        for (int j=1; j<=5;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
