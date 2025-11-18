e4b197fda6b33882fe001e8fa1b2be52551b1f42
vinxtluvvv <Vinit.26420@stu.upes.ac.in>
2025-11-18 19:35:34 +0530

    Add files via upload

---
+++ Q84.c
@@ -0,0 +1,21 @@
+/*Q84 (Strings)
+Convert a lowercase string to uppercase without using built-in functions. */
+
+#include <stdio.h>
+
+int main(){
+    char str[100];
+    int i = 0;
+    printf("Enter any string: \n");
+    scanf("%s", str);
+
+    while (str[i] != '\0'){
+        if (str[i] >= 'a' && str[i] <= 'z'){
+            str[i] = str[i] - 32;
+        }
+        i++;
+    }
+
+    printf("%s", str);
+    return 0;
+}
